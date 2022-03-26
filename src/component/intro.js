0130523a405b85a00f1eacac30221e38c764e8e8
10049 - Nguyễn Duy Lộc <locnd.blackwind@gmail.com>
2022-03-26 18:15:39 +0700

    update avatar

--- src/component/intro.js
+++ src/component/intro.js
@@ -35,7 +35,7 @@ function intro() {
                     <motion.div initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         transition={{ duration: 1, delay: 1 }} >
-                        <img src='https://scontent.fsgn5-11.fna.fbcdn.net/v/t1.6435-1/172003680_1151993241912709_1875434353264182821_n.jpg?stp=dst-jpg_p240x240&_nc_cat=111&ccb=1-5&_nc_sid=7206a8&_nc_ohc=1zt2da0G5v8AX_1Pe7t&_nc_ht=scontent.fsgn5-11.fna&oh=00_AT8SfOpbnhqFk8po9UtJaE1XdF_UQDtxZ6HahEM5NDcUpg&oe=62530B6C' />
+                        <img src='https://scontent.xx.fbcdn.net/v/t1.15752-9/275627078_284332510521685_3140852489407804615_n.jpg?stp=dst-jpg_p75x225&_nc_cat=103&ccb=1-5&_nc_sid=aee45a&_nc_ohc=FGvGOlN2DvsAX8kSDf0&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIt_xjPsqp_63HV2W1lGgk0ahL8297so8qooSr7rm1vTw&oe=62640FCA' />
                     </motion.div>
 
                 </div>
