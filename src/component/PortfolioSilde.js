9949a04f1242401506520d5a553011a252118c32
10049 - Nguyễn Duy Lộc <locnd.blackwind@gmail.com>
2022-03-27 15:26:42 +0700

    update portfolio data

--- src/component/PortfolioSilde.js
+++ src/component/PortfolioSilde.js
@@ -3,7 +3,7 @@ import { Swiper, SwiperSlide, } from 'swiper/react';
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-import SwiperCore, { Pagination} from "swiper";
+import SwiperCore, { Pagination } from "swiper";
 
 SwiperCore.use([Pagination]);
 function PortfolioSilde({ data }) {
@@ -31,7 +31,7 @@ function PortfolioSilde({ data }) {
             {...swiperConfig}
             pagination={true}
             className="swiper-card"
-            
+
         >
             {data.map((e, index) => {
                 return (
@@ -40,11 +40,12 @@ function PortfolioSilde({ data }) {
                     >
                         <div className="swiper-slide prortfolio-items">
                             <h3>{locale == "vn" ? "Tên dự án : " : "Project name : "} {e.project_name}</h3>
-                            {e.teamsize?<h4>{locale == "vn" ? "Số lượng thành viên : " : "Team size : "} {e.teamsize}</h4>:null}
-                            <h4>{locale == "vn" ? "Vị  trí phụ trách : " : "Responsiblities : "} {locale == "vn" ?e.desc_vn:e.desc_en}</h4>
-                            {e.tech?<h4>{locale == "vn" ? "Công nghệ : " : "Technologies : "} {e.tech}</h4>:null} 
-                            {e.url?<a href={`${e.url}`} target="_blank">{locale == "vn" ? "Đường dẫn : " : "Link : "}{e.url}</a>:null}
-                        
+                            <h4>{locale == "vn" ? "Mô tả dự án: " : "Project Description: "} {locale == "vn" ? e.desc_vn : e.desc_en}</h4>
+                            {e.teamsize ? <h4>{locale == "vn" ? "Số lượng thành viên : " : "Team size : "} {e.teamsize}</h4> : null}
+                            <h4>{locale == "vn" ? "Vị  trí phụ trách : " : "Responsiblities : "} {locale == "vn" ? e.responsiblities_vn : e.responsiblities_en}</h4>
+                            {e.tech ? <h4>{locale == "vn" ? "Công nghệ : " : "Technologies : "} {e.tech}</h4> : null}
+                            {e.url ? <a href={`${e.url}`} target="_blank">{locale == "vn" ? "Đường dẫn : " : "Link : "}{e.url}</a> : null}
+
                         </div>
                     </SwiperSlide>
                 )
--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,64 +2,88 @@ export const Portfolio = [
     {
         id: 1,
         project_name: "ChangeHouseShop",
-        desc_vn: "Kết nối với giao hàng tiết kiệm. Thực hiện tạo đơn hàng, đẩy đơn lên giao hàng tiết kiệm để giao hàng và tạo webhook nhận trạng thái từ giao hàng tiết kiệm.",
-        desc_en: "Connect with giaohangtietkiem. Make, push an order to giaohangtietkiem and create webhook to receive status from giaohangtietkiem.",
+        desc_en: "Build a  system using reactjs,nodejs and mysql to manage bill, product ,order ... and connect with giaohangtietkiem. Make, push an order to giaohangtietkiem and create webhook to receive status.",
+        desc_vn: "xây dựng hệ thống  sử dụng  reactjs, nodejs và mysql để quản lý hóa đơn, sản phẩm, đặt hàng ... và kết nối với giao hàng tiết kiệm. Thực hiện tạo đơn hàng, đẩy đơn lên giao hàng tiết kiệm để giao hàng và tạo webhook nhận trạng thái.",
+        responsiblities_vn: "Kết nối với giao hàng tiết kiệm. Thực hiện tạo đơn hàng, đẩy đơn lên giao hàng tiết kiệm để giao hàng và tạo webhook nhận trạng thái từ giao hàng tiết kiệm.",
+        responsiblities_en: "Connect with giaohangtietkiem. Make, push an order to giaohangtietkiem and create webhook to receive status from giaohangtietkiem.",
         tech: "#react-js, #webapi, #react-hook",
         teamsize: 2,
         url: ""
     }, {
         id: 2,
         project_name: "Skymond luxury",
-        desc_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
-        desc_en: "Get requirement from BA, create and fix modules of the system and website.",
+        desc_en: "Build a erp system using reactjs,nodejs and sql server to manage account, contact,product , order ... and build a website using nextjs to sell jewelry.",
+        desc_vn: "xây dựng hệ thống erp sử dụng reactjs, nodejs và sql để quản lý tài khoản, liên hệ, sản phẩm, đặt hàng ... và xây dựng trang web sử dụng nextjs để bán trang sức.",
+        responsiblities_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
+        responsiblities_en: "Get requirement from BA, create and fix modules of the system and website.",
         teamsize: 8,
         tech: "#reactjs, #nodejs, #sqlserver, #nextjs",
         url: "http://skymond-test.blackwind.vn/"
+    },{
+        id: 9,
+        project_name: "ungdungthansohoc",
+        desc_en: "Build a erp system using reactjs,nodejs and sql server to manage account, contact,product , order ... and build a website using nextjs to sell numerology product.",
+        desc_vn: "xây dựng hệ thống erp sử dụng reactjs, nodejs và sql để quản lý tài khoản, liên hệ, sản phẩm, đặt hàng ... và xây dựng trang web sử dụng nextjs để bán các sản phẩm của thần số học.",
+        responsiblities_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
+        responsiblities_en: "Get requirement from BA, create and fix modules of the system and website.",
+        teamsize: 6,
+        tech: "#reactjs, #nodejs, #sqlserver, #nextjs",
+        url: "https://ungdungthansohoc.com/"
     }, {
         id: 3,
         project_name: "cezan.edu",
-        desc_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
-        desc_en: "Get requirement from BA, create and fix modules of the system and website.",
+        desc_en: "Build a erp system using reactjs,nodejs and sql server to manage news, contact ... and build a website using nextjs",
+        desc_vn: "xây dựng hệ thống erp sử dụng reactjs, nodejs và sql để quản lý tin tức, liên hệ ... và xây dựng trang web sử dụng nextjs.",
+        responsiblities_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
+        responsiblities_en: "Get requirement from BA, create and fix modules of the system and website.",
         tech: "#reactjs, #nodejs, #sqlserver, #nextjs",
         teamsize: 4,
         url: "https://cezan.edu.vn/"
     }, {
         id: 4,
         project_name: "Inside Blackwind",
-        desc_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống. Tạo form chấm công và gửi email nội bộ cho nhận viên",
-        desc_en: "Get requirement from BA, create and fix modules of the system. Create a timekeeping module and  email module to use internal employees .",
+        desc_en: "Build a erp system using reactjs,nodejs and sql server to manage news, contact ... to use internal employees",
+        desc_vn: "xây dựng hệ thống erp sử dụng reactjs, nodejs và sql để quản lý tin tức, chấm công , email ... để sử dụng nội bộ",
+        responsiblities_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống. Tạo form chấm công và gửi email nội bộ cho nhận viên",
+        responsiblities_en: "Get requirement from BA, create and fix modules of the system. Create a timekeeping module and  email module to use internal employees .",
         tech: "#reactjs, #nodejs, #sqlserver",
         teamsize: 5,
         url: ""
     }, {
         id: 5,
         project_name: "Asin corona",
-        desc_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
-        desc_en: "Get requirement from BA, create and fix modules of the system and website.",
+        responsiblities_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
+        responsiblities_en: "Get requirement from BA, create and fix modules of the system and website.",
+        desc_en: "Build a erp system using reactjs,nodejs and sql server to manage news, contact,order ... and build a website using nextjs to sell nasal hygiene solution asin corona.",
+        desc_vn: "xây dựng hệ thống erp sử dụng reactjs, nodejs và sql để quản lý tin tức, liên hệ, đặt hàng ... và xây dựng trang web sử dụng nextjs để bán dung dịch vệ sinh mũi asin corona.",
         teamsize: 4,
         tech: "#reactjs, #nodejs, #sqlserver, #nextjs",
-        url: "https://asincorona.com/"
+        url: "beta.asincorona.com/"
     }, {
         id: 6,
         project_name: "I-housing",
-        desc_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
-        desc_en: "Get requirement from BA, create and fix modules of the system and website.",
+        responsiblities_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
+        responsiblities_en: "Get requirement from BA, create and fix modules of the system and website.",
+        desc_en: "Build a erp system using reactjs,nodejs and sql server to manage project,property,news, contact,order ... export excel file of contact list and build a website using nextjs to sell property.",
+        desc_vn: "xây dựng hệ thống erp sử dụng reactjs, nodejs và sql để quản lý dự án,bất động sản, liên hệ, đặt hàng ... xuất file excel danh sách liên hệ và xây dựng trang web sử dụng nextjs để bán bất động sản.",
         teamsize: 4,
         tech: "#reactjs, #nodejs, #sqlserver, #nextjs",
         url: "http://ihousing.vn/"
     },{
         id: 8,
         project_name: "Vigory",
-        desc_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
-        desc_en: "Get requirement from BA, create and fix modules of the system and website.",
+        responsiblities_vn: "Nhận yêu cầu từ BA ,tạo và sửa chữa các form khai báo hệ thống và website.",
+        responsiblities_en: "Get requirement from BA, create and fix modules of the system and website.",
+        desc_en: "Build a erp system using reactjs,nodejs and sql server to manage project,property,news, contact,order ... export excel file of contact list and build a website using nextjs to sell property.",
+        desc_vn: "xây dựng hệ thống erp sử dụng reactjs, nodejs và sql để quản lý dự án,bất động sản, liên hệ, đặt hàng ... xuất file excel danh sách liên hệ và xây dựng trang web sử dụng nextjs để bán bất động sản.",
         teamsize: 4,
         tech: "#reactjs, #nodejs, #sqlserver, #nextjs",
-        url: "Ongoing"
+        url: ""
     },{
         id: 7,
         project_name: "Mini projects",
-        desc_vn: "Vào thời gian rãnh tôi đã làm một vài thứ linh tinh",
-        desc_en: "In my free time, I work on some pet projects.",
+        responsiblities_vn: "Vào thời gian rãnh tôi đã làm một vài thứ linh tinh",
+        responsiblities_en: "In my free time, I work on some pet projects.",
         url: "https://github.com/nloc7770"
     }
 ]
